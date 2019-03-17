4cde77f89cdd278f5dee58fdb38eabbe42356cc9
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-03-17 12:52:23 +0100

    Remove unnecessary abstarction with virtual class

--- src/FSChanges.cc
+++ src/FSChanges.cc
@@ -8,16 +8,25 @@
 void writeSnapshotImpl(std::string *dir, std::string *snapshotPath, std::unordered_set<std::string> *ignore);
 EventList *getEventsSinceImpl(std::string *dir, std::string *snapshotPath, std::unordered_set<std::string> *ignore);
 
-class AsyncRunner {
+class FSAsyncRunner;
+typedef void (*AsyncFunction)(FSAsyncRunner *);
+
+class FSAsyncRunner {
 public:
-  void Queue() {
-    if(this->work) {
-      napi_status status = napi_queue_async_work(env, this->work);
-      NAPI_THROW_IF_FAILED_VOID(env, status);
-    }
-  }
-protected:
-  AsyncRunner(Napi::Env env): env(env) {
+  const Napi::Env env;
+
+  std::string directory;
+  std::string snapshotPath;
+  std::string backend;
+
+  std::unordered_set<std::string> ignore;
+  EventList *events;
+
+  FSAsyncRunner(Napi::Env env, Napi::Value dir, Napi::Value snap, Napi::Value opts, Napi::Promise::Deferred r, AsyncFunction func)
+    : env(env), directory(std::string(dir.As<Napi::String>().Utf8Value().c_str())),
+      snapshotPath(std::string(snap.As<Napi::String>().Utf8Value().c_str())),
+      events(nullptr), func(func), deferred(r) {
+
     napi_status status = napi_create_async_work(env, nullptr, env.Undefined(), 
                                                 OnExecute, OnWorkComplete, this, &this->work);
     if(status != napi_ok) {
@@ -29,66 +38,6 @@ protected:
       else
         Napi::Error::New(env).ThrowAsJavaScriptException();
     }
-  }
-  virtual ~AsyncRunner() {}
-  virtual void Execute() = 0;
-  virtual void OnOK() = 0;
-  virtual void OnError(const Napi::Error& e) = 0;
-  const Napi::Env env;
-
-private:
-  napi_async_work work;
-
-  static void OnExecute(napi_env env, void* this_pointer) {
-    AsyncRunner* self = (AsyncRunner*) this_pointer;
-    self->Execute();
-  }
-
-  static void OnWorkComplete(napi_env env, napi_status status, void* this_pointer) {
-    AsyncRunner* self = (AsyncRunner*) this_pointer;
-    if (status != napi_cancelled) {
-      Napi::HandleScope scope(self->env);
-      if(status == napi_ok) {
-        status = napi_delete_async_work(self->env, self->work);
-        if(status == napi_ok) {
-          self->OnOK();
-          delete self;
-          return;
-        }
-      }
-    }
-
-    // fallthrough for error handling
-    const napi_extended_error_info *error_info = 0;
-    napi_get_last_error_info(env, &error_info);
-    if(error_info->error_message){
-      self->OnError(Napi::Error::New(env, error_info->error_message));
-    } else {
-      self->OnError(Napi::Error::New(env));
-    }
-    delete self;
-  }
-};
-
-class FSAsyncRunner;
-typedef void (*AsyncFunction)(FSAsyncRunner *);
-
-class FSAsyncRunner : public AsyncRunner {
-public:
-  std::string directory;
-  std::string snapshotPath;
-  std::string backend;
-
-  std::unordered_set<std::string> ignore;
-  EventList *events;
-  Napi::Promise::Deferred deferred;
-  AsyncFunction func;
-
-  FSAsyncRunner(Napi::Env env, Napi::Value dir, Napi::Value snap, Napi::Value opts, Napi::Promise::Deferred r, AsyncFunction func)
-    : AsyncRunner(env), 
-      directory(std::string(dir.As<Napi::String>().Utf8Value().c_str())),
-      snapshotPath(std::string(snap.As<Napi::String>().Utf8Value().c_str())),
-      events(nullptr),  deferred(r), func(func) {
 
     if (opts.IsObject()) {
       Napi::Value v = opts.As<Napi::Object>().Get(Napi::String::New(env, "ignore"));
@@ -114,6 +63,49 @@ public:
       delete this->events;
     }
   }
+
+  void Queue() {
+    if(this->work) {
+      napi_status status = napi_queue_async_work(env, this->work);
+      NAPI_THROW_IF_FAILED_VOID(env, status);
+    }
+  }
+
+private:
+  napi_async_work work;
+  AsyncFunction func;
+  Napi::Promise::Deferred deferred;
+
+  static void OnExecute(napi_env env, void* this_pointer) {
+    FSAsyncRunner* self = (FSAsyncRunner*) this_pointer;
+    self->Execute();
+  }
+
+  static void OnWorkComplete(napi_env env, napi_status status, void* this_pointer) {
+    FSAsyncRunner* self = (FSAsyncRunner*) this_pointer;
+    if (status != napi_cancelled) {
+      Napi::HandleScope scope(self->env);
+      if(status == napi_ok) {
+        status = napi_delete_async_work(self->env, self->work);
+        if(status == napi_ok) {
+          self->OnOK();
+          delete self;
+          return;
+        }
+      }
+    }
+
+    // fallthrough for error handling
+    const napi_extended_error_info *error_info = 0;
+    napi_get_last_error_info(env, &error_info);
+    if(error_info->error_message){
+      self->OnError(Napi::Error::New(env, error_info->error_message));
+    } else {
+      self->OnError(Napi::Error::New(env));
+    }
+    delete self;
+  }
+
 
   void Execute() {
     this->func(this);
