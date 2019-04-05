7980af83d47c3f86a663d9b7a44a4c25dd8f6f1f
Devon Govett <devongovett@gmail.com>
2019-04-04 21:59:49 -0700

    Add JS wrapper around C++ binding

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-module.exports = require('bindings')('fschanges.node');
+const binding = require('bindings')('fschanges.node');
+const path = require('path');
+
+function normalizeOptions(opts = {}) {
+  if (Array.isArray(opts.ignore)) {
+    opts = Object.assign({}, opts, {
+      ignore: opts.ignore.map(ignore => path.resolve(ignore))
+    });
+  }
+
+  return opts;
+}
+
+exports.writeSnapshot = (dir, snapshot, opts) => {
+  return binding.writeSnapshot(path.resolve(dir), path.resolve(snapshot), normalizeOptions(opts));
+};
+
+exports.getEventsSince = (dir, snapshot, opts) => {
+  return binding.getEventsSince(path.resolve(dir), path.resolve(snapshot), normalizeOptions(opts));
+};
+
+exports.subscribe = async (dir, fn, opts) => {
+  dir = path.resolve(dir);
+  opts = normalizeOptions(opts);
+  await binding.subscribe(dir, fn, opts);
+
+  return {
+    unsubscribe() {
+      return binding.unsubscribe(dir, fn, opts);
+    }
+  };
+};
+
+exports.unsubscribe = (dir, fn, opts) => {
+  return binding.unsubscribe(path.resolve(dir), fn, normalizeOptions(opts));
+};
