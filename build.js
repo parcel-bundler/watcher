6246e67d41909d1a8d8873054ec2e8afe5b2e121
Jasper De Moor <jasperdemoor@gmail.com>
2019-12-15 17:06:54 +0100

    dont build docker images on os x

--- build.js
+++ build.js
@@ -4,8 +4,8 @@ const { execSync } = require("child_process");
 // windows, mac and modern linux
 execSync("npm run prebuild:current");
 
-// Run the docker builds on os x and linux
-if (os.platform() === "darwin" || os.platform() === "linux") {
+// Run the docker builds on linux
+if (os.platform() === "linux") {
   // centos (old libc) and alpine
   execSync("npm run prebuild:docker");
 }
