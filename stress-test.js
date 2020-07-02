67323afb96255eedf15b98300fdd86ca923e1da0
Jasper De Moor <jasperdemoor@gmail.com>
2020-07-02 21:29:19 +0200

    up the stress

--- stress-test.js
+++ stress-test.js
@@ -26,9 +26,9 @@ async function run(backend) {
     console.log('Running stress test:', currentRunId);
     await fs.remove(stressTestDir);
     await Promise.all(
-      new Array(25).fill('').map(async (_, dirName) => {
+      new Array(100).fill('').map(async (_, dirName) => {
         let dir = path.join(stressTestDir, dirName.toString(10));
-        for (let filename = 0; filename < 100; filename++) {
+        for (let filename = 0; filename < 250; filename++) {
           let filepath = path.join(dir, filename.toString(10));
           await fs.outputFile(filepath, '');
         }
