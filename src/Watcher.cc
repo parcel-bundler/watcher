6b19232ce3f92a84e04f37378288b84df830569a
Aaron Lichtman <alichtman@meta.com>
2025-09-30 05:54:33 -0700

    Fix another SIOF issue
    Related to https://github.com/parcel-bundler/watcher/pull/208. We noticed
    another similar stack trace in our monitoring stack, and this patch seems to
    resolve it

--- src/Watcher.cc
+++ src/Watcher.cc
@@ -15,30 +15,33 @@ struct WatcherCompare {
   }
 };
 
-static std::unordered_set<WatcherRef , WatcherHash, WatcherCompare> sharedWatchers;
+static std::unordered_set<WatcherRef , WatcherHash, WatcherCompare>& getSharedWatchers() {
+  static std::unordered_set<WatcherRef , WatcherHash, WatcherCompare> sharedWatchers;
+  return sharedWatchers;
+}
 
 WatcherRef Watcher::getShared(std::string dir, std::unordered_set<std::string> ignorePaths, std::unordered_set<Glob> ignoreGlobs) {
   WatcherRef watcher = std::make_shared<Watcher>(dir, ignorePaths, ignoreGlobs);
-  auto found = sharedWatchers.find(watcher);
-  if (found != sharedWatchers.end()) {
+  auto found = getSharedWatchers().find(watcher);
+  if (found != getSharedWatchers().end()) {
     return *found;
   }
 
-  sharedWatchers.insert(watcher);
+  getSharedWatchers().insert(watcher);
   return watcher;
 }
 
 void removeShared(Watcher *watcher) {
-  for (auto it = sharedWatchers.begin(); it != sharedWatchers.end(); it++) {
+  for (auto it = getSharedWatchers().begin(); it != getSharedWatchers().end(); it++) {
     if (it->get() == watcher) {
-      sharedWatchers.erase(it);
+      getSharedWatchers().erase(it);
       break;
     }
   }
 
   // Free up memory.
-  if (sharedWatchers.size() == 0) {
-    sharedWatchers.rehash(0);
+  if (getSharedWatchers().size() == 0) {
+    getSharedWatchers().rehash(0);
   }
 }
 
